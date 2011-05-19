cff15cab183b0737d73e62822f2f99ef2d467ed7
gathreya <gathreya@80492ad4-71fa-11dd-8bb7-e6bbbfb71c85>
2011-05-19 18:53:06 +0000

    KCIRB-1372: Applied to the demo data of the 3.1 release sprint 3 scripts.
    git-svn-id: https://svn.kuali.org/repos/kc/branches/kc_project-3_1_M3@12096 80492ad4-71fa-11dd-8bb7-e6bbbfb71c85

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP3-SCRIPT/demo-data/KR-demo-Demo-ORACLE.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP3-SCRIPT/demo-data/KR-demo-Demo-ORACLE.sql
@@ -3,5 +3,7 @@ set sqlblanklines on
 spool KR-demo-Demo-ORACLE-Install.log
 @ORACLE/KR_DEMO1_KRIM_ENTITY_AFLTN_T.sql
 @ORACLE/KR_DEMO1_KRIM_GRP_MBR_T.sql
+@ORACLE/KR_DEMO1_KRIM_ROLE_MBR_ATTR_DATA_T.sql
+@ORACLE/KR_DEMO2_KRIM_ROLE_MBR_T.sql
 commit;
 exit
