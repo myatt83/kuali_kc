c808bbd4b0d0aabc3699ee5748c631cd22aa6cca
kbtaylor <kbtaylor@80492ad4-71fa-11dd-8bb7-e6bbbfb71c85>
2011-05-19 14:57:03 +0000

    Moving sponsor form templates to demo data
    git-svn-id: https://svn.kuali.org/repos/kc/branches/kc_project-3_1_M3@12094 80492ad4-71fa-11dd-8bb7-e6bbbfb71c85

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP3-SCRIPT/demo-data/KC-demo-Demo-MYSQL.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP3-SCRIPT/demo-data/KC-demo-Demo-MYSQL.sql
@@ -1,2 +1,3 @@
+\. MYSQL/KC_DEMO1_SPONSOR_FORM_TEMPLATES.sql
 commit;
 exit
--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP3-SCRIPT/demo-data/KC-demo-Demo-ORACLE.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP3-SCRIPT/demo-data/KC-demo-Demo-ORACLE.sql
@@ -1,5 +1,6 @@
 set define off
 set sqlblanklines on
 spool KC-demo-Demo-ORACLE-Install.log
+@ORACLE/KC_DEMO1_SPONSOR_FORM_TEMPLATES.sql
 commit;
 exit
