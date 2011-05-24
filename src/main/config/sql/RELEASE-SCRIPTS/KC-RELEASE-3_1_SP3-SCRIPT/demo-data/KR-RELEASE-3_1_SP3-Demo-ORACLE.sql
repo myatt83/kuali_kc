fd074953dbf2b83828008f5bcc085fbfeb3afc9f
kbtaylor <kbtaylor@80492ad4-71fa-11dd-8bb7-e6bbbfb71c85>
2011-05-24 18:59:36 +0000

    Fixed demo data creation scripts
    git-svn-id: https://svn.kuali.org/repos/kc/branches/kc_project-3_1_M3@12141 80492ad4-71fa-11dd-8bb7-e6bbbfb71c85

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP3-SCRIPT/demo-data/KR-RELEASE-3_1_SP3-Demo-ORACLE.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP3-SCRIPT/demo-data/KR-RELEASE-3_1_SP3-Demo-ORACLE.sql
@@ -1,6 +1,6 @@
 set define off
 set sqlblanklines on
-spool KR-demo-Demo-ORACLE-Install.log
+spool KR-RELEASE-3_1_SP3-Demo-ORACLE-Install.log
 @ORACLE/KR_DEMO1_KRIM_ENTITY_AFLTN_T.sql
 @ORACLE/KR_DEMO1_KRIM_GRP_MBR_T.sql
 @ORACLE/KR_DEMO1_KRIM_ROLE_MBR_ATTR_DATA_T.sql
