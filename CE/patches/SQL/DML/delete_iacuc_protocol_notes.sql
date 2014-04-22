b68075c4acbfd44b59ac99b6e742078d805e3453
Lance Speelmon <lance@speelmon.com>
2014-04-22 15:08:07 -0700

    Added some notes on SQL

---
+++ CE/patches/SQL/DML/delete_iacuc_protocol_notes.sql
@@ -0,0 +1,17 @@
+select * from iacuc_protocol;
+
+delete from iacuc_principles;
+delete from iacuc_protocol_actions;
+delete from iacuc_protocol_attach_protocol;
+delete from iacuc_protocol_funding_source;
+delete from iacuc_protocol_location;
+delete from iacuc_protocol_notepad;
+delete from iacuc_protocol_persons;
+delete from iacuc_protocol_research_areas;
+delete from iacuc_proc_person_responsible;
+delete from iacuc_protocol_study_groups;
+delete from iacuc_protocol_species;
+delete from iacuc_protocol_study_group_dtl;
+delete from iacuc_protocol_study_group_hdr;
+delete from iacuc_proto_study_group_locs;
+delete from  iacuc_protocol;
