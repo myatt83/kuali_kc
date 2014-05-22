c9ae9cd7edcdca979e7cd3aed9c015ee15d8a419
Lance Speelmon <lance@speelmon.com>
2014-05-22 16:33:01 -0700

    Added missing cleanup of iacuc_protocol_document

--- CE/patches/SQL/DML/remove_all_transactional_data.sql
+++ CE/patches/SQL/DML/remove_all_transactional_data.sql
@@ -44,6 +44,11 @@ delete from iacuc_proto_study_group_locs;
 delete from comm_schedule_minutes;
 delete from iacuc_protocol;
 
+delete from KRNS_DOC_HDR_T where DOC_HDR_ID IN (select DOCUMENT_NUMBER from iacuc_protocol_document);
+delete from krew_actn_itm_t where DOC_HDR_ID IN (select DOCUMENT_NUMBER from iacuc_protocol_document);
+delete from krew_doc_hdr_t where DOC_HDR_ID IN (select DOCUMENT_NUMBER from iacuc_protocol_document);
+delete from iacuc_protocol_document;
+
 -- select * from protocol;
 delete from protocol_correspondence;
 delete from protocol_actions;
